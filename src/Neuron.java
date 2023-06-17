ed37f9ce7ec10b7300db4c995a26af3667583ef0
Kvdl <evkachmala@gmail.com>
2023-06-18 00:15:05 +0700

    new repo

---
+++ src/Neuron.java
@@ -0,0 +1,12 @@
+public abstract class Neuron {
+    final private Double weight;
+    final private Double bias;
+
+    private Neuron[] next_layer;
+    private double output = 0;
+
+    public void responseInput(Double val) {}
+
+    private void actiate() {}
+    
+}
